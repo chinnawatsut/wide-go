c11db71d561e1e7c0fa7c993ddd0f57847ef20d4
Chinnawat K <chinnawatsut@gmail.com>
2017-09-16 22:29:58 +0700

    just init inteface

--- interfacing/main.go
+++ interfacing/main.go
@@ -2,6 +2,37 @@ package main
 
 import "fmt"
 
+type animal interface {
+	run()
+	eat()
+	walk()
+}
+
+type dog struct {
+	name string
+}
+
+func (d dog) run() {
+	fmt.Println("RUN")
+
+}
+
+func (d dog) eat() {
+	fmt.Println("EAT")
+}
+
+func (d dog) walk() {
+	fmt.Println("WALK")
+
+}
+
+func allTheyCanDo(a animal) {
+	a.eat()
+	a.run()
+	a.walk()
+}
+
 func main() {
-	fmt.Println("HELLO WITH INTERFACE :) ")
+	rogers := dog{"roger"}
+	allTheyCanDo(rogers)
 }
